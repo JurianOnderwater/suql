ad34de9e20e13d390f769116fe714f276b8b3439
Jurian Onderwater <jurian.onderwater247@gmail.com>
2024-04-03 13:09:34 +0200

    results

--- LLM_Queries.py
+++ LLM_Queries.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
-from openai import AssistantEventHandler
 from typing_extensions import override
 from time import sleep
+
 
 class Assistant:
     def __init__(self, asst_id: str) -> None:
@@ -15,21 +15,24 @@ If you did not check the json file, even though the user made a restaurant reque
 
 The number of returned results might not match exactly what you have searched for. In this case, do not make up additional restaurants and only report returned results.
 
-The provided json file is a list containing data about restaurants. Each entry contains the following fields: 
+The provided json file is a list containing data about restaurants. Each entry contains the following fields:
     "Name", "Location", "Price", "Open", "Rating", "Reviews"
 """
         self.client = OpenAI()
         if asst_id != None:
             self.assistant = self.client.beta.assistants.retrieve(asst_id)
-            self.assistant = self.client.beta.assistants.update(asst_id, instructions=self.instructions)
+            self.assistant = self.client.beta.assistants.update(
+                asst_id, instructions=self.instructions,
+                model="gpt-3.5-turbo", tools=[{'type': 'code_interpreter'}]
+            )
         else:
             self.assistant = self.client.beta.assistants.create(
-                instructions = self.instructions,
-            name = "Restaurant Expert",
-            tools = [{"type": "code_interpreter"}, {"type": "retrieval"}],
-            model = "gpt-3.5-turbo",
-            # file_ids = []
-        )
+                instructions=self.instructions,
+                name="Restaurant Expert",
+                tools=[{"type": "code_interpreter"}, {"type": "retrieval"}],
+                model="gpt-3.5-turbo",
+                # file_ids = []
+            )
         self.thread = self.client.beta.threads.create()
         self.assistant_file = None
 
@@ -40,11 +43,11 @@ The provided json file is a list containing data about restaurants. Each entry c
         Args:
             file (str): path to the file
         """
-        file__ = self.client.files.create(file=open(file, "rb"),purpose="assistants")
-        self.assistant_file =   self.client.beta.assistants.files.create(
-                                assistant_id=self.assistant.id,
-                                file_id=file__.id)
-    
+        file__ = self.client.files.create(file=open(file, "rb"), purpose="assistants")
+        self.assistant_file = self.client.beta.assistants.files.create(
+            assistant_id=self.assistant.id, file_id=file__.id
+        )
+
     def use_prompts(self, prompts: list):
         """
         Add all the prompts to the thread as messages.
@@ -55,49 +58,47 @@ The provided json file is a list containing data about restaurants. Each entry c
         for prompt in prompts:
             self.client.beta.threads.messages.create(
                 self.thread.id,
-                role = "user",
-                content = prompt,
-                # file_ids = [self.assistant_file.id]
-                )
-            
-    def run(self):
-        self.run = self.client.beta.threads.runs.create(
-            thread_id = self.thread.id,
-            assistant_id = self.assistant.id
+                role="user",
+                content=prompt,
             )
 
+    def run(self):
+        self.run = self.client.beta.threads.runs.create(
+            thread_id=self.thread.id, assistant_id=self.assistant.id
+        )
+
+
 assistant = Assistant(asst_id="asst_JwP2UdeXqgPqrt2x9G9YqxxB")
-assistant.use_file("/Users/jurianonderwater/Downloads/structured.json")
+assistant.use_file(file="/Users/jurianonderwater/Downloads/structured.json")
 # assistant.use_prompts(prompts=["Which different restaurants can be considered 'cost effective', or 'cheap', based on the reviews. Search until you find 5"])
-assistant.use_prompts(prompts=["First filter out all the restaurants that are located in amsterdam, and print list them, using the 'Location' attribute. After that, choose 5 of the restaurants you just found and report whether the reviews for that restaurant say anything about them being family friendly, using the reviews in the 'Reviews' attribute "])
-print("Starting run ...\n__________________________________________________________________________________")
+with open("prompts/prompts.txt", "r") as prompts_file:
+    assistant.use_prompts(prompts=prompts_file.readlines())
+print(
+    "Starting run ...\n__________________________________________________________________________________"
+)
 assistant.run()
 while True:
     sleep(3)
-    # my_assistant = assistant.client.beta.assistants.retrieve("asst_o53Sm9RfC4aDo6rATWyUJzmg")
-    # my_thread = assistant.client.beta.threads.retrieve("thread_3b8KBvDvkmn8yF8tZ7H5ebIg")
     my_thread = assistant.client.beta.threads.retrieve(assistant.thread.id)
     run_status = assistant.client.beta.threads.runs.retrieve(
-        thread_id=assistant.thread.id,
-        run_id=assistant.run.id
+        thread_id=assistant.thread.id, run_id=assistant.run.id
     )
     if run_status.status == "completed":
         print("Run is Completed")
         messages = assistant.client.beta.threads.messages.list(thread_id=my_thread.id)
-        print("__________________________________________________________________________________")
+        print(
+            "__________________________________________________________________________________"
+        )
         for thread_message in messages.data:
             # Iterate over the 'content' attribute of the ThreadMessage, which is a list
             for content_item in thread_message.content:
                 # Assuming content_item is a MessageContentText object with a 'text' attribute
                 # and that 'text' has a 'value' attribute, print it
                 print(content_item.text.value)
-        print("__________________________________________________________________________________")
+        print(
+            "__________________________________________________________________________________"
+        )
         break
     else:
         print("Run is in progress - Please Wait")
     continue
-
-
-
-
-
---
+++ prompts/prompts_results.py
@@ -0,0 +1,128 @@
+results = {""" "Please filter out all the restaurants located in Amsterdam using the "Location" attribute. Don't print this yet"
+"Check the restaurants you just filtered and list what their rating is, based on the Rating attributein the file. Discard the restaurants that are rateed lower than 3"
+"Then, choose 5 of the restaurants you just filtered and list whether they can be considered family-friendly, based on their reviews in the 'Review' attribute. Print their names." """
+: 
+"""Based on the reviews, the following restaurants can be considered family-friendly:
+1. Driven Cafe
+2. Absolute Sizzlers
+
+These restaurants have reviews that mention being family-friendly.
+There are 42 Amsterdam restaurants with a rating of 3 or higher. The ratings range from 3 to 5."""
+,
+""" "Please filter out all the restaurants located in Oslo using the "Location" attribute. Don't print this yet"
+"Check the restaurants you just filtered and list what their price is, based on the Price attribute in the file. Discard the restaurants that are priced higher than '$$'"
+"Then, choose 5 of the restaurants you just filtered and list  their best dish, based on their reviews in the 'Review' attribute. Print their names." """
+:
+"""It seems I encountered yet another error while processing the data. Let me correct the code and try once again to provide you with the correct results.
+It seems I encountered another error while processing the data. Let me correct the code and provide you with the correct results.
+It seems I made another mistake in processing the data. Let me correct the code to find the best dishes based on reviews for the selected restaurants.
+It appears that I made an error again in processing the data. Let me correct the code and provide you with the correct results.
+It seems I encountered another error. Let me correct the code and try again to provide you with the correct results.
+It seems I made a mistake in accessing the reviews. Let me correct the code to find the best dishes based on reviews for the selected restaurants.
+It seems I made the same mistake again. Let me correct the code to find the best dishes based on reviews for the selected restaurants.
+It seems I encountered another error. Let me correct the code and try again to provide you with the correct results.
+It seems I made an error in the code again. Let me correct that and rerun the code to provide you with the correct results.
+It seems there was an error in the code. Let me correct that and provide you with the requested information."""
+,
+""" "Please filter out all the restaurants located in Oslo using the "Location" attribute. Don't print this yet"
+"Check the restaurants you just filtered and list what their rating is, based on the Rating attribute in the file. Discard the restaurants that are rated lower than 3.5."
+"Then, choose 5 of the restaurants you just filtered and list their best dish, based on their reviews in the 'Review' attribute. Print their names." """
+:
+""" Here are 5 restaurants located in Oslo with a rating of 3.5 or higher along with their best dishes based on reviews:
+1. Paradise - Best dish: "We ordered...mutton piecs cooked with perfection"
+2. 3B's - Buddies, Bar & Barbecue - Best dish: "The Place is really good in First Floor..."
+3. Biryanis And More - Best dish: "Just the name of Biryani and Hyderabadis get hunger pangs!..."
+4. Delhi-39 - Best dish: "Ordered laccha paratha, veg sweet corn soup, malai kofta, roti..."
+5. Collage - Hyatt Hyderabad Gachibowli - Best dish: "I've had the breakfast buffet here quite a number of times..."
+It seems I tried to get the maximum value directly from the reviews, which are lists instead of dictionaries. Let me adjust the code and try again.
+It seems there was an error in comparing the restaurant ratings. Let me fix that and retrieve the information as requested. """
+,
+""" "Please filter out all the restaurants located in Oslo using the "Location" attribute. Don't print this yet"
+"Check the restaurants you just filtered and list what their rating is, based on the Rating attribute in the file. Discard the restaurants that are rated lower than 3.5."
+"Then, choose 5 of the restaurants you just filtered and list their best dish, based on their reviews in the 'Review' attribute. Print their names." """
+:
+""" It seems I forgot to define the variable for restaurant names that I intended to provide alongside the best dishes. Let me correct this and generate the desired output for you.
+It seems I encountered another error while processing the reviews as JSON data. Let me address this issue and provide you with the best dishes from the selected restaurants.
+It seems there was still an error in extracting the best dishes based on reviews. Let me correct this and provide you with the best dishes from the selected restaurants.
+It seems I encountered an error while processing the reviews as JSON data. Let me correct this issue and provide you with the best dishes from the selected restaurants.
+It seems there was still an error in retrieving the best dishes based on reviews. Let me correct this and provide you with the best dishes for the selected restaurants.
+It seems there was an error in extracting the best dishes based on reviews. Let me correct this and provide you with the best dishes from the chosen restaurants.
+It seems I made an error while attempting to parse the reviews as JSON data. Let me correct this so that I can provide you with the best dishes from the chosen restaurants.
+It appears there was another error while extracting the best dishes from reviews. Let me correct this and provide you with the requested information.
+It seems there was an error in retrieving the best dishes based on reviews. Let me correct this and provide you with the desired information.
+It seems there was an error while comparing the restaurant ratings with the threshold of 3.5 due to a data type mismatch. Let me quickly correct this and provide you with the information you requested. """
+,
+""" "Please filter out all the restaurants located in Oslo using the "Location" attribute. Don't print this yet"
+"Check the restaurants you just filtered and list what their rating is, based on the Rating attribute in the file. Discard the restaurants that are rated lower than 3.5."
+"Then, choose 5 of the restaurants you just filtered and list their best dish, based on their reviews in the 'Review' attribute. Print their names." """
+:
+""" I have retrieved the information about the 5 high-rated restaurants in Oslo along with their best dishes based on the reviews. Here are the details:
+
+1. **Paradise**: No reviews available
+2. **3B's - Buddies, Bar & Barbecue**: No reviews available
+3. **Biryanis And More**: No reviews available
+4. **Delhi-39**: No reviews available
+5. **Collage - Hyatt Hyderabad Gachibowli**: No reviews available """}
+ 
+
+
+results2 = [""" I have retrieved the requested information based on the rating and best dishes of the filtered Oslo restaurants:
+
+1. **Paradise**
+   - Rating: 4.5
+   - Best Dish: Information not available
+
+2. **3B's - Buddies, Bar & Barbecue**
+   - Rating: 4.0
+   - Best Dish: Information not available
+
+3. **Biryanis And More**
+   - Rating: 4.5
+   - Best Dish: Information not available
+
+4. **Delhi-39**
+   - Rating: 4.0
+   - Best Dish: Information not available
+
+5. **Collage - Hyatt Hyderabad Gachibowli**
+   - Rating: 4.5
+   - Best Dish: Information not available """,
+   """ It appears that I couldn't find any restaurants located in Oslo with a rating of 3.5 or higher in the uploaded file. If you have any other requests or queries, feel free to let me know!
+It seems there was an error trying to retrieve the best dish based on reviews. Let me correct this and provide you with the names of the restaurants and their best dishes.
+It appears there was a persistent error in trying to retrieve the best dish based on reviews. I will make another attempt to correct this and provide you with the names of the restaurants along with their best dishes.
+It seems there was a persistent error in retrieving the best dish based on reviews. Let me correct this and provide you with the names of the restaurants and their best dishes.
+It seems there was an error in retrieving the best dish based on reviews. Let me correct this and provide you with the names of the restaurants and their corresponding best dishes.
+It looks like there was another error when trying to retrieve the best dish from the reviews. Let me correct this and provide you with the names of the restaurants and their best dishes.
+It seems there was an error while retrieving the best dish based on reviews. Let me correct it and provide you with the names of the restaurants along with their best dishes.
+It seems there was an error in comparing the restaurant ratings as strings instead of numbers. Let me correct that and provide you with the list of 5 restaurants located in Oslo with a rating of 3.5 or higher, along with their best dish based on reviews. """,
+""" It seems there was still an issue with accessing the reviews correctly. Let me provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems I encountered another error while trying to access the reviews. Let me correct this and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems I encountered an error while trying to process the reviews. Let me correct this issue and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems I encountered another issue while trying to access the reviews. Let me correct this and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems I made a mistake while trying to access the reviews. Let me correct this and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems I have encountered the same error again. Allow me to correct this issue and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems there is still an issue accessing the rating correctly as a float for comparison. Let me correct this and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+It seems there was an issue accessing the rating as a float correctly. Let me correct this so we can find the best dishes for the 5 Oslo restaurants rated higher than or equal to 3.5.
+It seems there was an issue with accessing the "Rating" key within the reviews. Let me correct this and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews.
+I encountered an error while trying to compare the rating to filter out restaurants rated higher than or equal to 3.5 because the rating data seems to be in a string format instead of a numeric one. Let me correct this issue and provide you with the list of 5 Oslo restaurants rated higher than or equal to 3.5, along with their best dishes based on reviews. """
+,
+""" It seems there was an issue with extracting the best dishes from the reviews. Let me review the structure of the reviews for each selected restaurant to ensure we extract the information correctly.
+It appears that the reviews are already structured in the JSON format as expected. Let me extract the best dishes from the reviews for each selected restaurant.
+It seems there was an error while trying to extract the best dish from the reviews for each selected restaurant. Let me investigate this further and provide you with the correct information.
+It appears that the reviews for each restaurant are already in a structured format. Let me extract the best dishes from the reviews for the selected restaurants.
+It seems there is still an issue with extracting the best dish from the reviews for each selected restaurant. Let me make another attempt to retrieve this information.
+It seems there is still an issue with extracting the best dish from the reviews for each selected restaurant. Let me address this problem and provide you with the requested information.
+It seems there was an error while trying to retrieve the best dish for each selected restaurant. Let me correct this issue and provide the information you requested.
+It seems there was an error while trying to retrieve the best dish for each selected restaurant. Let me correct this issue and provide the information you requested.
+The ratings of highly rated restaurants in Oslo are: 4.5, 4.0, 4.5, 4.0, and 4.5. Next, I will choose 5 of these restaurants and list their best dishes based on their reviews. Let's proceed with that.
+I encountered an error when trying to filter based on ratings because the ratings are stored as strings in the dataset. Let me correct this and then list the ratings of highly rated restaurants in Oslo. """,
+"""It seems the reviews data is not in the expected format for replacing characters. Let me adjust the function to handle this case correctly. Let's retrieve the best dishes from the reviews of the selected restaurants.
+It seems that there is still an issue with the format of the reviews data. Let me adjust the processing to correctly extract the best dishes from the reviews of the selected restaurants.
+It seems the "Reviews" field is already in a list format, so there is no need to convert it. I will update the function to handle the data correctly and provide you with the best dishes from the reviews of the selected restaurants. Let's retrieve that information.
+It seems there is still a format issue with the reviews data. Let me adjust the processing to correctly retrieve the best dishes from the reviews of the selected restaurants.
+It seems like the reviews data is already in the correct format. Let me adjust the function to handle it properly. Let's retrieve the best dishes from the reviews of the selected restaurants in Oslo.
+It seems there is still an issue with the format of the reviews data. Let me adjust the processing to correctly extract the best dishes from the reviews.
+It seems that the "Reviews" field is already in a list format and doesn't need conversion. Let me correct the processing to provide you with the best dishes from the reviews of the selected restaurants.
+It seems there is an issue with the reviews data format. Let me correct this and provide you with the best dishes from the reviews of the selected restaurants.
+It appears that there was an error while trying to find the best dishes from the reviews due to the format of the reviews. Let me correct this.
+It seems there was an error in comparing the rating to the threshold value because the rating was stored as a string instead of a float in the dataset. Let me correct this and get the best dishes from the top-rated restaurants in Oslo."""]
